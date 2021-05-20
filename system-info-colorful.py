f9b8cac3b0867e9604b9ddfaadc2f2cbf02659eb
Ahmet Efe AKYAZI <46342237+efe3535@users.noreply.github.com>
2021-05-20 21:15:53 +0300

    Added bicim.py, system-info-colorful.py files.
    Kolay gele!

---
+++ system-info-colorful.py
@@ -0,0 +1,9 @@
+import platform
+from bicim import *
+
+os = " ".join(platform.dist()).capitalize()
+system = platform.system()
+kernel_version = platform.release()
+username = platform.uname()
+
+print(f"Merhaba, {kirmizi} {username.node} {sıfır}\n{mavi}bu işletim sistemini: {os},{sıfır}\n{pembe}bu çekirdeği: {system},{sıfır}\n{yesil}bu çekirdek versiyonunu: {kernel_version}, {sıfır}\nkullanıyor olmalısın.")
