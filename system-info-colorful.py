69e17c32e8f9d3f4062c30230dcfba69e9879608
Ahmet Efe AKYAZI <46342237+efe3535@users.noreply.github.com>
2021-05-29 22:07:28 +0300

    Fixed problems.
    Sorunlar düzeltildi, inan bana. Küçücüüük. :+1:

--- system-info-colorful.py
+++ system-info-colorful.py
@@ -1,10 +1,12 @@
 import platform
 import distro
 from bicim import *
+import getpass
 
 os = " ".join(distro.linux_distribution(full_distribution_name=False)).capitalize()
 system = platform.system()
 kernel_version = platform.release()
-username = platform.uname()
+# username = platform.uname() Shows computer name instead of username. 
+username = getpass.getuser()
 
 print(f"Merhaba, {kirmizi} {username.node} {sıfır}\n{mavi}bu işletim sistemini: {os},{sıfır}\n{pembe}bu çekirdeği: {system},{sıfır}\n{yesil}bu çekirdek versiyonunu: {kernel_version}, {sıfır}\nkullanıyor olmalısın.")
